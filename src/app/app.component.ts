1abf23411a1962224a7dbaf92eded6868dc8d3c7
bmuszynska <beata.muszynska.it@gmail.com>
2024-09-30 20:10:59 +0200

    Add app component
    Main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'voting-app-client';
+  public candidates: string[] = [
+    "Joker",
+    "Harley Quinn",
+    "Penguin",
+    "Riddler",
+    "Two-Face",
+    "Scarecrow",
+    "Poison Ivy",
+    "Bane",
+    "Mr. Freeze",
+    "Ra's al Ghul"
+  ];
+  public data_candidates = [
+    { name: "Joker", voteCount: 0 },
+    { name: "Harley Quinn", voteCount: 0 },
+    { name: "Penguin", voteCount: 0 },
+    { name: "Riddler", voteCount: 0 },
+    { name: "Two-Face", voteCount: 0 },
+    { name: "Scarecrow", voteCount: 0 },
+    { name: "Poison Ivy", voteCount: 0 },
+    { name: "Bane", voteCount: 0 },
+    { name: "Mr. Freeze", voteCount: 0 },
+    { name: "Ra's al Ghul", voteCount: 0 }
+  ];
+  public data_voters = [
+    { "hasVoted": "true", "name": "Luke Skywalker" },
+    { "hasVoted": "false", "name": "Darth Vader" },
+    { "hasVoted": "true", "name": "Princess Leia" },
+    { "hasVoted": "true", "name": "Han Solo" },
+    { "hasVoted": "true", "name": "Yoda" },
+    { "hasVoted": "false", "name": "Obi-Wan Kenobi" },
+    { "hasVoted": "false", "name": "Chewbacca" },
+    { "hasVoted": "false", "name": "R2-D2" },
+    { "hasVoted": "true", "name": "C-3PO" },
+    { "hasVoted": "true", "name": "Emperor Palpatine" }
+  ];
+  public title = 'voting-app-client';
+  public voters: string[] = [
+    "Luke Skywalker",
+    "Darth Vader",
+    "Princess Leia",
+    "Han Solo",
+    "Yoda",
+    "Obi-Wan Kenobi",
+    "Chewbacca",
+    "R2-D2",
+    "C-3PO",
+    "Emperor Palpatine"
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormControl, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CandidatesComponent } from './candidates/candidates.component';
+import { DataTableComponent } from './data-table/data-table.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { VotersComponent } from './voters/voters.component';
+import { VotingStatsComponent } from './voting-stats/voting-stats.component';
+import { VotingComponent } from './voting/voting.component';
+import { NewPersonComponent } from './new-person/new-person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent,
+    VotingComponent,
+    VotersComponent,
+    CandidatesComponent,
+    DataTableComponent,
+    VotingStatsComponent,
+    NewPersonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatTableModule,
+    FlexLayoutModule,
+    MatGridListModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
